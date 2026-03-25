ab58efc639bc8ce82fa5432d2e99a0ffd14dc9d2
Omar Gedawy <omargedawy@outlook.com>
2026-03-25 20:29:34 +0200

    added new files

--- main_tavily_search.py
+++ main_tavily_search.py
@@ -1,6 +1,3 @@
-from typing import List
-
-from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,21 +7,10 @@ from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
-class Source(BaseModel):
-    """Schema for a source used by the agent"""
-
-    url:str = Field(description="The url of the source")
-
-class AgentResponse(BaseModel):
-    """Schema for the agent response with answer and sources"""
-
-    answer: str = Field(description="The agent's answer to the question")
-    sources: List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
-
 
 llm = ChatOpenAI(model='gpt-5')
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
+agent = create_agent(model=llm, tools=tools)
 
 def main():
     print("Hello from my-langchain-course!")
